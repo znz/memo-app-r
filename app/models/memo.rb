d4b6064a76f709ff5d49580fd0ceadea4f3ef31a
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2018-05-06 17:15:47 +0900

    Update tags in form

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -2,5 +2,18 @@
 
 # Memo data
 class Memo < ApplicationRecord
+  acts_as_taggable_array_on :tags
   belongs_to :user
+
+  validates :user, presence: true
+  private def validate_tags
+    if !tags.is_a?(Array) || tags.any?(&:empty?)
+      errors.add(:tags, :invalid)
+    end
+  end
+
+  private def unique_tags
+    self.tags = tags.uniq.sort.reject(&:empty?)
+  end
+  before_validation :unique_tags
 end
