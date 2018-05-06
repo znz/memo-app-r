36c6a2dc3bb3640baa9501beec68301af032e427
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2018-05-06 14:58:27 +0900

    Add missing helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,26 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = I18n.t(:'nav.site_name')
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
+  def nav_link_to(name, path)
+    active = request.path == path
+    nav_item_html_class = active ? 'nav-item active' : 'nav-item'
+    content_tag(:li, class: nav_item_html_class) do
+      link_to path, class: 'nav-link' do
+        if active
+          h(name) + content_tag(:span, '(current)', class: 'sr-only')
+        else
+          name
+        end
+      end
+    end
+  end
 end
