c487478c9059870b800e5edac033c7cc45f44383
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2018-05-06 14:51:39 +0900

    Generate StaticPagesController
    `rails g controller StaticPages home about`

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/about'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
