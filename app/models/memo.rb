8deabe8903428094fead9ef14392be4d76e509fd
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2018-05-06 16:12:45 +0900

    Add relation

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
+# Memo data
 class Memo < ApplicationRecord
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@
 # User information
 class User < ApplicationRecord
   devise(:confirmable, :database_authenticatable, :lockable, :recoverable, :registerable, :rememberable, :timeoutable, :trackable, :validatable)
+  has_many :memos, dependent: :destroy
 end
