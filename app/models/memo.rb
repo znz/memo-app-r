4aca9e91c9085297bcc9854a240c7ecf8e2a0758
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2018-06-18 14:40:45 +0900

    Fix errors

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -7,13 +7,15 @@ class Memo < ApplicationRecord
 
   validates :user, presence: true
   private def validate_tags
-    if !tags.is_a?(Array) || tags.any?(&:empty?)
+    if !tags.is_a?(Array) || tags.any?(&:blank?)
       errors.add(:tags, :invalid)
     end
   end
 
   private def unique_tags
-    self.tags = tags.uniq.sort.reject(&:empty?)
+    if tags.is_a?(Array)
+      self.tags = tags.uniq.sort.reject(&:blank?)
+    end
   end
   before_validation :unique_tags
 end
