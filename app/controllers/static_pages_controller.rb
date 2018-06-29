a89d01488781081dd216e6c2590b6464a23f799d
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2018-06-29 20:16:25 +0900

    Rename static_pages to pages and update nav

--- app/controllers/static_pages_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# static pages
-class StaticPagesController < ApplicationController
-  def home; end
-
-  def about; end
-end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -2,18 +2,13 @@
 
 require 'test_helper'
 
-class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+class PagesControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
   setup do
     user = users(:one)
     user.confirm
     sign_in user
-  end
-
-  test 'should get home' do
-    get root_url
-    assert_response :success
   end
 
   test 'should get about' do
