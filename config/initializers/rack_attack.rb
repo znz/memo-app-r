22c1b68d549bf4d174a59f3302d3c7246844f44a
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2019-08-02 20:56:23 +0900

    Fix offences

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -5,8 +5,8 @@ Rails.application.config.middleware.use Rack::Attack
 Rack::Attack.blocklist('fail2ban pentesters') do |req|
   Rack::Attack::Fail2Ban.filter("pentesters-#{req.ip}", maxretry: 3, findtime: 10.minutes, bantime: 5.minutes) do
     CGI.unescape(req.query_string) =~ %r{/etc/passwd} ||
-    req.path.include?('/etc/passwd') ||
-    req.path.include?('wp-admin') ||
-    req.path.include?('wp-login')
+      req.path.include?('/etc/passwd') ||
+      req.path.include?('wp-admin') ||
+      req.path.include?('wp-login')
   end
 end
