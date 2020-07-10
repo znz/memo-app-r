e96548d5863dce7eacfad0cd5f37a6843c32dc09
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2020-07-11 00:25:22 +0900

    Fix Performance/StringInclude

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -4,7 +4,7 @@ Rails.application.config.middleware.use Rack::Attack
 
 Rack::Attack.blocklist('fail2ban pentesters') do |req|
   Rack::Attack::Fail2Ban.filter("pentesters-#{req.ip}", maxretry: 3, findtime: 10.minutes, bantime: 5.minutes) do
-    CGI.unescape(req.query_string) =~ %r{/etc/passwd} ||
+    CGI.unescape(req.query_string).include?('/etc/passwd') ||
       req.path.include?('/etc/passwd') ||
       req.path.include?('wp-admin') ||
       req.path.include?('wp-login')
