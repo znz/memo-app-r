201d850cb63db7d6d4d515696fb54e1c3784ef53
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2020-10-21 23:16:14 +0900

    Auto-correct Layout/ExtraSpacing

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.generators do |g|
   g.orm :active_record, primary_key_type: :uuid
-  g.helper       false
-  g.javascripts  false
-  g.stylesheets  false
+  g.helper false
+  g.javascripts false
+  g.stylesheets false
 end
