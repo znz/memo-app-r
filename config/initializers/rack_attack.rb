8f95fe5d170d12811da294b422c387df7ea7f158
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2020-12-09 18:35:46 +0900

    Add `.htm` to rack-attack

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -6,6 +6,7 @@ Rack::Attack.blocklist("fail2ban pentesters") do |req|
   Rack::Attack::Fail2Ban.filter("pentesters-#{req.ip}", maxretry: 3, findtime: 10.minutes, bantime: 5.minutes) do
     CGI.unescape(req.query_string).include?("/etc/passwd") ||
       req.path.include?("/etc/passwd") ||
+      req.path.end_with?(".htm") ||
       req.path.include?("wp-admin") ||
       req.path.include?("wp-login")
   end
