66345179aedb1399b25db09d69745cb8dd437ad2
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2023-01-27 19:10:15 +0900

    rubocop auto correct offences

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@
 
 module ApplicationHelper
   def full_title(page_title = "")
-    base_title = I18n.t(:'nav.site_name')
+    base_title = I18n.t(:"nav.site_name")
     if page_title.empty?
       base_title
     else
