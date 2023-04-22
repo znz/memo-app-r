80a36882bb4e2555b383b77e03750af49fd59e72
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2023-04-22 18:57:44 +0900

    Merge `rails app:update`

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative "boot"
 
 ENV["RANSACK_FORM_BUILDER"] = "::SimpleForm::FormBuilder"
@@ -15,10 +13,13 @@ module MemoAppR
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
 
     config.time_zone = "Asia/Tokyo"
   end
