7ae05050d13bf6b3a512a0bd0758643fd0b41b86
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2023-11-24 15:28:02 +0900

    Merge `bin/rails app:update`

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "hoge_production"
+  # config.active_job.queue_name_prefix = "memo_app_r_production"
 
   config.action_mailer.perform_caching = false
 
