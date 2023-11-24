9ace16d209726fdcc803e7342fd3769999845798
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2023-11-24 15:33:33 +0900

    Convert using `decaffeinate`
    https://github.com/decaffeinate/decaffeinate
    https://decaffeinate-project.org/

---
+++ app/assets/javascripts/pos.js
@@ -0,0 +1,57 @@
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
+ */
+$(function() {
+  const locupdate = function(pos) {
+    const info = document.getElementById("memo_info");
+    const lonlat = document.getElementById("memo_lonlat");
+    const debug = document.getElementById("debug");
+    if (info) {
+      info.value =
+        "(lat:" + pos.coords.latitude +
+        ",lon:" + pos.coords.longitude +
+        ",acc:" + pos.coords.accuracy +
+        ")";
+    }
+    if (lonlat) {
+      lonlat.value =
+        "POINT(" + pos.coords.longitude +
+        " " + pos.coords.latitude +
+        ")";
+    }
+    if (debug) {
+      return debug.innerHTML =
+        "lat : " + pos.coords.latitude +
+        "<br/>long : " + pos.coords.longitude +
+        "<br/>accuracy : " + pos.coords.accuracy + "<br/>";
+    }
+  };
+  const handleError = function(a) {
+    const debug = document.getElementById("debug");
+    if (debug) {
+      return debug.innerHTML = "<p> error: " + a.code + "</p>";
+    }
+  };
+  let id = null;
+  const start = function() {
+    if (id) {
+      return;
+    }
+    const new_memo = document.getElementById("new_memo");
+    if (new_memo) {
+      return id = navigator.geolocation.watchPosition(locupdate, handleError);
+    }
+  };
+  const stop = function() {
+    if (!id) {
+      return;
+    }
+    navigator.geolocation.clearWatch(id);
+    return id = null;
+  };
+  $(document).on('turbolinks:load', start);
+  $(document).on('turbolinks:click', stop);
+  return start();
+});
