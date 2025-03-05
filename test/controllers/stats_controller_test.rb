b6e36d0be075dd8230f8779a16a47221b1aecc18
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2025-03-05 22:12:14 +0900

    Auto correct Layout/SpaceInsideHashLiteralBraces,Style/StringLiterals

--- test/controllers/stats_controller_test.rb
+++ test/controllers/stats_controller_test.rb
@@ -4,12 +4,12 @@ class StatsControllerTest < ActionDispatch::IntegrationTest
   def authorization = ActionController::HttpAuthentication::Basic.encode_credentials(ENV["STATS_BASIC_AUTH_USER"], ENV["STATS_BASIC_AUTH_PASS"])
 
   test "should get gc" do
-    get stats_gc_url(format: :json), headers: { 'HTTP_AUTHORIZATION' => authorization }
+    get stats_gc_url(format: :json), headers: {"HTTP_AUTHORIZATION" => authorization}
     assert_response :success
   end
 
   test "should get yjit" do
-    get stats_yjit_url(format: :json), headers: { 'HTTP_AUTHORIZATION' => authorization }
+    get stats_yjit_url(format: :json), headers: {"HTTP_AUTHORIZATION" => authorization}
     assert_response :success
   end
 end
