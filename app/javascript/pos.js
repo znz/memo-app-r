1a790835ec49eefbdfe076a6e6fa42a42688dcca
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2026-03-25 17:48:47 +0900

    Fix indent

--- app/javascript/pos.js
+++ app/javascript/pos.js
@@ -4,54 +4,54 @@
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
  */
 
-  const locupdate = function(pos) {
-    const info = document.getElementById("memo_info");
-    const lonlat = document.getElementById("memo_lonlat");
-    const debug = document.getElementById("debug");
-    if (info) {
-      info.value =
-        "(lat:" + pos.coords.latitude +
-        ",lon:" + pos.coords.longitude +
-        ",acc:" + pos.coords.accuracy +
-        ")";
-    }
-    if (lonlat) {
-      lonlat.value =
-        "POINT(" + pos.coords.longitude +
-        " " + pos.coords.latitude +
-        ")";
-    }
-    if (debug) {
-      return debug.innerHTML =
-        "lat : " + pos.coords.latitude +
-        "<br/>long : " + pos.coords.longitude +
-        "<br/>accuracy : " + pos.coords.accuracy + "<br/>";
-    }
-  };
-  const handleError = function(a) {
-    const debug = document.getElementById("debug");
-    if (debug) {
-      return debug.innerHTML = "<p> error: " + a.code + "</p>";
-    }
-  };
-  let id = null;
-  const start = function() {
-    if (id) {
-      return;
-    }
-    const new_memo = document.getElementById("new_memo");
-    if (new_memo) {
-      return id = navigator.geolocation.watchPosition(locupdate, handleError);
-    }
-  };
-  const stop = function() {
-    if (!id) {
-      return;
-    }
-    navigator.geolocation.clearWatch(id);
-    return id = null;
-  };
+const locupdate = function(pos) {
+  const info = document.getElementById("memo_info");
+  const lonlat = document.getElementById("memo_lonlat");
+  const debug = document.getElementById("debug");
+  if (info) {
+    info.value =
+      "(lat:" + pos.coords.latitude +
+      ",lon:" + pos.coords.longitude +
+      ",acc:" + pos.coords.accuracy +
+      ")";
+  }
+  if (lonlat) {
+    lonlat.value =
+      "POINT(" + pos.coords.longitude +
+      " " + pos.coords.latitude +
+      ")";
+  }
+  if (debug) {
+    return debug.innerHTML =
+      "lat : " + pos.coords.latitude +
+      "<br/>long : " + pos.coords.longitude +
+      "<br/>accuracy : " + pos.coords.accuracy + "<br/>";
+  }
+};
+const handleError = function(a) {
+  const debug = document.getElementById("debug");
+  if (debug) {
+    return debug.innerHTML = "<p> error: " + a.code + "</p>";
+  }
+};
+let id = null;
+const start = function() {
+  if (id) {
+    return;
+  }
+  const new_memo = document.getElementById("new_memo");
+  if (new_memo) {
+    return id = navigator.geolocation.watchPosition(locupdate, handleError);
+  }
+};
+const stop = function() {
+  if (!id) {
+    return;
+  }
+  navigator.geolocation.clearWatch(id);
+  return id = null;
+};
 
-  document.addEventListener('turbo:load', start);
-  document.addEventListener('turbo:click', stop);
-  start();
+document.addEventListener('turbo:load', start);
+document.addEventListener('turbo:click', stop);
+start();
